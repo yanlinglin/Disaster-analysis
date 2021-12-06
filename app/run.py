38a98b24ec7bbfd5b7b058cd23ab724818169040
Y.Lin <38507020+yanlinglin@users.noreply.github.com>
2021-12-06 16:27:36 -0600

    add comments, visualization

--- app/run.py
+++ app/run.py
@@ -7,7 +7,7 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+from plotly.graph_objs import Bar,Pie
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
@@ -43,14 +43,24 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    df1 = df.drop(['id','message','original','genre'], axis=1)
+    category_counts=df1.sum(axis=0)
+    category_names = df1.columns
+    
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
+#             'data': [
+#                 Bar(
+#                     x=genre_names,
+#                     y=genre_counts
+#                 )
+#             ],
+             'data': [
+                Pie(
+                    labels=genre_names,
+                    values=genre_counts
                 )
             ],
 
@@ -61,6 +71,24 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category"
                 }
             }
         }
--- data/process_data.py
+++ data/process_data.py
@@ -6,28 +6,54 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
+    
+    """
+    Load  datasets from 2 filepaths, merge and save as a SQL file.
+    
+    Parameters:
+    messages_filepath: messages.csv
+    categories_filepath: categories.csv
+    
+    Returns:
+    df: dataframe containing messages_filepath and categories_filepath merged
+    
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath) 
-    df = messages.merge(categories, how='inner', on=['id'])
+    df = messages.merge(categories, how='inner', on=['id']) #- Merge the messages and categories datasets using the common id
     return df
 
 def clean_data(df):
-    categories_col = df.categories.str.split(';',expand=True)
-    row = categories_col.iloc[0]
+    
+    """
+    clean the data to prepare for machine learning model    
+    Parameters:
+    df: the merged data set 
+    
+    Returns:
+    df: dataframe with cleaned-up columns
+    
+    """
+    categories_col = df.categories.str.split(';',expand=True) #Split the values in the categories column on the ; character so that each value becomes a separate column.
+    row = categories_col.iloc[0] #Use the first row of categories dataframe to create column names for the categories data.
     category_colnames = row.apply(lambda x: x[0:len(x)-2])
-    categories_col.columns = category_colnames
+    categories_col.columns = category_colnames #Rename columns of categories with new column names.
     for column in categories_col:
         # set each value to be the last character of the string
         categories_col[column]=categories_col[column].apply(lambda x: x[-1])
         # convert column from string to numeric
         categories_col[column] = categories_col[column].astype(int)
-    categories_col['related'].replace(2,0,inplace=True)
-    df.drop('categories',axis=1,inplace=True)
+    categories_col['related'].replace(2,0,inplace=True)  #replace output's 2 by 0
+    df.drop('categories',axis=1,inplace=True) # drop the original "categories" column 
     df = pd.concat([df,categories_col],axis=1)
     df.drop_duplicates(inplace=True)
     return df
 
 def save_data(df, database_filename):
+    """
+    save the cleaned-up data to the target location: database_filename
+    
+    """
     engine = create_engine('sqlite:///'+ database_filename)
     df.to_sql('df', engine, index=False,if_exists='replace')  
 
